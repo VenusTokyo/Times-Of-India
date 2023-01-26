730ce33f83dc03e8b2184d2d55ec627bd522eaf7
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-01-26 22:06:01 +0530

    footer imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Navbar, Header} from './components'
+import {Navbar, Header, Footer} from './components'
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/> */}
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
