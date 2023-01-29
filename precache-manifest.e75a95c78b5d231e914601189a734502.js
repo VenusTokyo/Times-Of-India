50072df456d1dccd17c731aca8e9eb4de64f1d8a
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2023-01-29 19:57:33 +0530

    Updates

---
+++ precache-manifest.e75a95c78b5d231e914601189a734502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357a90a0f68032a45d60db3102060b65",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "de553d15c864204c215a",
+    "url": "/react-gh-pages/static/css/main.2c67101e.chunk.css"
+  },
+  {
+    "revision": "17bc98f421c11aad1d45",
+    "url": "/react-gh-pages/static/js/2.55cf5571.chunk.js"
+  },
+  {
+    "revision": "de553d15c864204c215a",
+    "url": "/react-gh-pages/static/js/main.4bc7430a.chunk.js"
+  },
+  {
+    "revision": "a66bfc9012f2338d172e",
+    "url": "/react-gh-pages/static/js/runtime~main.7e606fd1.js"
+  }
+]);
